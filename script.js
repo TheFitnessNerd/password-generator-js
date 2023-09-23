2f1c30b2d47834ad1536e42ffe5cd57866306a96
michellemartinez <mm_1984@ymail.com>
2023-09-23 11:10:45 -0500

    debugged and finished code

--- script.js
+++ script.js
@@ -2,8 +2,48 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var length = prompt("Enter Password. 8-128 characters")
-  var characters = promt("Use a combination of uppercase letters, lowercase letters, and special characters")
+  var length = parseInt(prompt("Choose a length of at least 8 but no more than 128 charcters."));
+
+  if (length < 8 || length > 128) {
+  alert("Invalid length")
+  return;
+  }
+
+  var useLowerCase = confirm("Would you like to use lowercase letters?");
+  var useUpperCase = confirm("Would you like to use uppercase letters?");
+  var useNumeric = confirm("Would you like to use numbers?");
+  var useSpecial = confirm("Would you like to use special characters?");
+
+if (!useLowerCase && !useUpperCase && !useNumeric && !useSpecial){
+  
+  alert("Please select at least one character type for your password");
+  generatePassword();
+
+} else {
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?";
+
+var allChars = ``
+  if (useLowerCase) allChars += lowercaseChars;
+  if (useUpperCase) allChars += uppercaseChars;
+  if (useNumeric) allChars += numberChars;
+  if (useSpecial) allChars += specialChars;
+// console.log(allChars);
+selectedCharsArray = [...allChars];
+// console.log(selectedCharsArray);
+let randomPassword = "";
+for (let i = 0; i < length; i++) {
+//  console.log(selectedCharsArray[i])
+let randomIndex = Math.floor(Math.random() * selectedCharsArray.length);
+// console.log(randomIndex);
+// console.log(selectedCharsArray[randomIndex]);
+randomPassword = randomPassword + selectedCharsArray[randomIndex];
+
+}
+return randomPassword;
+}
 }
 
 // Write password to the #password input
@@ -18,22 +58,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-console.log()
 
-
-
-
-// const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-
-// function generatePassword(length) {
-//   let password = "";
-//   for (let i = 0; i < length; i++) {
-//     const randomIndex = Math.floor(Math.random() * chars.length);
-//     password += chars[randomIndex];
-//   }
-//   return password;
-// }
-
-// // Example usage:
-// const generatedPassword = generatePassword(12);
-// console.log(generatedPassword);
